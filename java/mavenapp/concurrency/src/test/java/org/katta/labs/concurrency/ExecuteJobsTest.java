da6ef6d2133c0359850482a138758e4bf76311e1
Srivatsa Katta <vatsa.katta@gmail.com>
2011-10-15 19:41:35 +0530

    JobStatus introduced to check if job executed

--- java/mavenapp/concurrency/src/test/java/org/katta/labs/concurrency/ExecuteJobsTest.java
+++ java/mavenapp/concurrency/src/test/java/org/katta/labs/concurrency/ExecuteJobsTest.java
@@ -1,5 +1,6 @@
 package org.katta.labs.concurrency;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ExecuteJobsTest {
@@ -8,13 +9,28 @@ public class ExecuteJobsTest {
     public void shouldExecuteAJob() {
         BatchExecutor batchExecutor = new BatchExecutor();
 
+        final JobStatus jobStatus = new JobStatus();
+
         batchExecutor.addJob(new MyJob() {
             @Override
             public void execute() {
-                System.out.println("executed");
+                jobStatus.setExecuted(true);
             }
         });
 
         batchExecutor.execute();
+        Assert.assertTrue(jobStatus.isExecuted());
+    }
+
+    class JobStatus {
+        private boolean executed;
+
+        public boolean isExecuted() {
+            return executed;
+        }
+
+        public void setExecuted(boolean executed) {
+            this.executed = executed;
+        }
     }
 }
