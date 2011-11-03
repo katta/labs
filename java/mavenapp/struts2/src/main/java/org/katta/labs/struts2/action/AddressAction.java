db663eeb0200fb4f3088f1b8d724a3944a3bb7fa
Srivatsa Katta <vatsa.katta@gmail.com>
2011-11-03 22:29:17 +0530

    added form with field validations on server side

--- java/mavenapp/struts2/src/main/java/org/katta/labs/struts2/action/AddressAction.java
+++ java/mavenapp/struts2/src/main/java/org/katta/labs/struts2/action/AddressAction.java
@@ -1,10 +1,47 @@
 package org.katta.labs.struts2.action;
 
+import com.google.common.base.Strings;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class AddressAction extends ActionSupport {
 
+    private String firstName;
+    private String lastName;
+
+
     public String show() throws InterruptedException {
         return "show_address_form";
+    }
+
+    public void validateSave() {
+
+        if(Strings.isNullOrEmpty(getFirstName())) {
+            addFieldError("firstName", "First name is mandatory.");
+        }
+
+        if(Strings.isNullOrEmpty(getLastName())) {
+            addFieldError("lastName", "Last name is mandatory.");
+        }
+    }
+
+    public String save() {
+        addActionMessage("Successfully saved!!");
+        return SUCCESS;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
--- java/mavenapp/struts2/src/main/webapp/address_form.jsp
+++ java/mavenapp/struts2/src/main/webapp/address_form.jsp
@@ -1,3 +1,37 @@
-<div>
-    this is address form
-</div>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<%@ taglib prefix="sj" uri="/struts-jquery-tags" %>
+
+<html>
+<head>
+    <sj:head jqueryui="true"/>
+
+    <style type="text/css">
+        .errorMessage {
+            color: #FF0000;
+        }
+    </style>
+
+</head>
+<body>
+    <div style="text-align: center">
+        <h2>Address Form</h2>
+    </div>
+
+    <div>
+        <s:actionmessage />
+
+        <s:form action="saveAddress" >
+
+            <s:textfield name="firstName" label="First Name" required="true" />
+            <s:textfield name="lastName" label="Last Name" />
+
+
+            <s:submit />
+
+
+        </s:form>
+
+    </div>
+
+</body>
+</html>
